e1b199f5644693ed10b3d9c455d5b527d0de66d1
Oana Peter <oanapeter29@gmail.com>
2024-04-10 17:05:40 +0300

    influencer+subscriptions

--- Iss/Entity/BasicSubscription.cs
+++ Iss/Entity/BasicSubscription.cs
@@ -8,5 +8,15 @@ namespace Iss.Entity
 {
     public class BasicSubscription : ISubscription
     {
+        private int numberOfCampaigns {  get; set; }
+        private decimal price { get; set; }
+        private int reach { get; set; }
+
+        public BasicSubscription(int numberOfCampaigns, decimal price, int reach)
+        {
+            this.numberOfCampaigns = numberOfCampaigns;
+            this.price = price;
+            this.reach = reach;
+        }
     }
 }
--- Iss/Entity/SilverSubscription.cs
+++ Iss/Entity/SilverSubscription.cs
@@ -8,5 +8,15 @@ namespace Iss.Entity
 {
     public class SilverSubscription : ISubscription
     {
+        private int numberOfCampaigns { get; set; }
+        private decimal price { get; set; }
+        private int reach { get; set; }
+
+        public SilverSubscription(int numberOfCampaigns, decimal price, int reach)
+        {
+            this.numberOfCampaigns = numberOfCampaigns;
+            this.price = price;
+            this.reach = reach;
+        }
     }
 }
