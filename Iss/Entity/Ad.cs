c0af7277a7171f671c99505da3fb88b0e5221f20
IrinaOltean14 <115115597+IrinaOltean14@users.noreply.github.com>
2024-04-11 13:35:24 +0300

    Update Ad and AdSet classes

--- Iss/Entity/Ad.cs
+++ Iss/Entity/Ad.cs
@@ -8,5 +8,21 @@ namespace Iss.Entity
 {
     public class Ad
     {
+        public string productName { get; set; }
+        public List<string> photos { get; set; }
+        public List<string> videos { get; set; }
+        public string description { get; set; }
+        public string websiteLink {  get; set; }
+
+        public Ad() { }
+        public Ad(string productName, List<string> mediaContent, string description, string websiteLink)
+        {
+            productName = productName;
+            mediaContent = mediaContent;
+            description = description;
+            websiteLink = websiteLink;
+        }
+
+
     }
 }
--- Iss/Entity/AdSet.cs
+++ Iss/Entity/AdSet.cs
@@ -8,5 +8,23 @@ namespace Iss.Entity
 {
     public class AdSet
     {
+        public List<Ad> ads { get;  set; }
+        public List<string> targetAudience { get; set; }
+        public AdSet() { }
+        public AdSet(List<Ad> ads, List<string> targetAudience)
+        {
+            ads = ads;
+            targetAudience = targetAudience;
+        }
+
+        public void addAd(Ad ad)
+        {
+            ads.Add(ad);
+        }
+
+        public void removeAd(Ad ad)
+        {
+            ads.Remove(ad);
+        }
     }
 }
