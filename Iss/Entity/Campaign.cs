2fc2694c44a204e6eae3f74d9741d1a22cbc995e
Florin1616 <115114026+Florin1616@users.noreply.github.com>
2024-04-11 17:43:04 +0300

    completed campaign

--- Iss/Entity/Campaign.cs
+++ Iss/Entity/Campaign.cs
@@ -8,5 +8,19 @@ namespace Iss.Entity
 {
     public class Campaign
     {
+        public string Name { get; set; }
+        public string TargetAudience { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Duration { get; set; }
+
+        public Campaign() { }
+
+        public Campaign(string name, string targetAudience, DateTime startDate, int duration)
+        {
+            Name = name;
+            TargetAudience = targetAudience;
+            StartDate = startDate;
+            Duration = duration;
+        }
     }
 }
