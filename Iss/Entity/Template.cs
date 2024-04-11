3311ec0ce84506431c3c8ac476e4deab908bafc9
sorana134 <sorana.resiga@stud.ubbcluj.ro>
2024-04-11 22:02:54 +0300

    added getter, setter, constructor

--- Iss/Entity/Template.cs
+++ Iss/Entity/Template.cs
@@ -8,5 +8,23 @@ namespace Iss.Entity
 {
     public class Template
     {
+        private string title;
+        private string description;
+        private string theme;
+        private string photo;
+        //getters and setters
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public string Theme { get => theme; set => theme = value; }
+        public string Photo { get => photo; set => photo = value; }
+        //constructor
+        public Template(string title, string description, string theme, string photo)
+        {
+            this.title = title;
+            this.description = description;
+            this.theme = theme;
+            this.photo = photo;
+        }
+
     }
 }
