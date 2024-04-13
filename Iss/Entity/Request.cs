9517417fb71b7fd18924946441cb96d112313697
Georgiana Petricele <115110913+georgianapetricele@users.noreply.github.com>
2024-04-13 13:00:43 +0300

    Make request gui added and update Request class

--- Iss/Entity/Request.cs
+++ Iss/Entity/Request.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace Iss.Entity
 {
-    public class Request(string purpose, string campaignOverview, string contentRequirements, string compensationPackage, string collaborationFee, string timeline, string legalAgreements)
+    public class Request(string purpose, string campaignOverview, string contentRequirements, string compensationPackage, string collaborationFee, DateTime startDate, DateTime endDate, string legalAgreements)
     {
         public string purpose { get; set; } = purpose;
         public string campaignOverview { get; set; } = campaignOverview;
         public string contentRequirements { get; set; } = contentRequirements;
         public string compensationPackage { get; set; } = compensationPackage;
         public string collaborationFee { get; set; } = collaborationFee;
-        public string timeline { get; set; } = timeline;
+        public DateTime startDate { get; set; } = startDate;
+        public DateTime endDate { get; set; } = endDate;
+
         public string legalAgreements { get; set; } = legalAgreements;
     }
 }
