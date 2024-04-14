ceece38cb4f86d4b3a399a3d77a25d056f105c6f
915-PascaAlexS <alexpasca1122@gmail.com>
2024-04-14 10:49:37 +0300

    Created database connection

---
+++ Iss/Repository/DatabaseConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Iss.Repository
+{
+    public class DatabaseConnection
+    {
+        private static string ConnectionString = "Data Source=DESKTOP-DDRKBC8\\SQLEXPRESS;Initial Catalog=db_ISS;Integrated Security=True";
+
+        public SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
